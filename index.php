61c2aea0f7d45c1be1088c3dc43f246112223f94
slu000uls <sitesoheil@gmail.com>
2023-09-15 19:13:12 +0430

    explain

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ include 'Lexica.php';
 
 $lexica = new Lexica("dog");
 //put the text you want to search in Lexica (Line 5)
-$lexica->json_data(); 
+$lexica->json_data(); //You can Get your Data With This
