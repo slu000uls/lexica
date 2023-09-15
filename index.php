91c0c6e6bba01dab77eb5cb35be21925a1792f8d
slu000uls <sitesoheil@gmail.com>
2023-09-15 19:23:47 +0430

    add some text

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ include 'Lexica.php';
 
 $lexica = new Lexica("dog");
 //put the text you want to search in Lexica (Line 5)
+
 $lexica->json_data(); //You can Get your Data With This
+
+//You can create a Telegram bot where users can easily find the photo they want.
